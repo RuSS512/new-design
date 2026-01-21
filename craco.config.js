606b84a9736a2e72a09341516824b426910c0128
RuSS512 <vip.rusel2@gmail.com>
2026-01-22 02:13:14 +0300

    Create craco.config.js

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: {
+    configure: (config) => {
+      config.resolve = config.resolve || {};
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        process: require.resolve("process/browser"),
+      };
+
+      config.plugins = [
+        ...(config.plugins || []),
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        }),
+      ];
+
+      return config;
+    },
+  },
+};
